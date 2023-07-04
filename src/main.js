1b8417812036b5b0134d5ba63f670b9354449d71
irwanphan <irwanphan@gmail.com>
2023-07-05 01:24:52 +0700

    clear codes with eslint

--- src/main.js
+++ src/main.js
@@ -1,72 +1,59 @@
-import axios from 'axios';
-import _ from 'lodash';
-import { ApodImage, NeoCard } from './components.js';
+import axios from "axios";
+import { ApodImage, NeoCard } from "./components.js";
 
 // my NASA API key
-const apiKey = 'rrfdoFQbSconvzMXfpu3EJwfqW0eEiIGbc8FSX39';
+const apiKey = "rrfdoFQbSconvzMXfpu3EJwfqW0eEiIGbc8FSX39";
 
 // Function to fetch APOD image from NASA API
 const fetchApodImage = () => {
-    const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`;
+  const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`;
 
-    axios
-        .get(apiUrl)
-        .then(response => {
-            const { url, title } = response.data;
-            const apodContainer = document.getElementById('apodContainer');
-            const apodImage = new ApodImage(apodContainer);
-            apodImage.updateImage(url, title);
-        })
-        .catch(error => {
-            console.error('Error fetching APOD image:', error);
-        });
+  axios
+    .get(apiUrl)
+    .then((response) => {
+      const { url, title } = response.data;
+      const apodContainer = document.getElementById("apodContainer");
+      const apodImage = new ApodImage(apodContainer);
+      apodImage.updateImage(url, title);
+    })
+    .catch((error) => {
+      console.error("Error fetching APOD image:", error);
+    });
 };
 
 // Function to fetch NEOs from NASA API
 const fetchNEOs = () => {
-    const apiUrl = `https://api.nasa.gov/neo/rest/v1/feed?api_key=${apiKey}`;
-  
-    axios
-        .get(apiUrl)
-        .then(response => {
-            const neoData = response.data;
-            const neoList = neoData.near_earth_objects;
-    
-            const cardContainer = document.getElementById('neoCardContainer');
-            cardContainer.innerHTML = ''; // Clear the container before adding new cards
+  const apiUrl = `https://api.nasa.gov/neo/rest/v1/feed?api_key=${apiKey}`;
 
-            // Create cards for each NEO
-            for (const date in neoList) {
-            neoList[date].forEach(neo => {
-                const neoCard = new NeoCard(neo);
-                cardContainer.appendChild(neoCard.getCardElement());
-            });
-            }
-        })
-        .catch(error => {
-            console.error('Error fetching NEOs:', error);
+  axios
+    .get(apiUrl)
+    .then((response) => {
+      const neoData = response.data;
+      const neoList = neoData.near_earth_objects;
+
+      const cardContainer = document.getElementById("neoCardContainer");
+      cardContainer.innerHTML = "";
+
+      // Create cards for each NEO
+      for (const date in neoList) {
+        neoList[date].forEach((neo) => {
+          const neoCard = new NeoCard(neo);
+          cardContainer.appendChild(neoCard.getCardElement());
         });
+      }
+    })
+    .catch((error) => {
+      console.error("Error fetching NEOs:", error);
+    });
 };
 
-// Button click event listener
-// function handleReloadButtonClick() {
-//     fetchApodImage();
-// }
+// Initialize application
+document.addEventListener("DOMContentLoaded", () => {
+  // const apodContainer = document.getElementById("apodContainer");
+  // const apodImage = new ApodImage(apodContainer);
 
-// Initialize the application
-document.addEventListener('DOMContentLoaded', () => {
-    const apodContainer = document.getElementById('apodContainer');
-    const apodImage = new ApodImage(apodContainer);
-
-    // Reload APOD image :) not used anymore
-    // const reloadButton = document.createElement('button');
-    // reloadButton.textContent = 'Reload Image';
-    // reloadButton.addEventListener('click', handleReloadButtonClick);
-    // apodContainer.appendChild(reloadButton);
-
-    // Fetch the initial APOD image
-    fetchApodImage();
-
-    // function to fetch NEOs
-    fetchNEOs();
+  // Fetch initial APOD image
+  fetchApodImage();
+  // fetch NEOs
+  fetchNEOs();
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,33 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    mode: 'development',
-    entry: './src/main.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    module: {
-        rules: [
-        {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader',
-            },
+  mode: "development",
+  entry: "./src/main.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/dist/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
         },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-            },
-        ],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
     },
-    devServer: {
-        static: {
-          directory: path.resolve(__dirname, 'dist'),
-        },
-        compress: true,
-        port: 8000,
-    },
+    compress: true,
+    port: 8000,
+  },
 };
